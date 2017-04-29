babe938db63c35e6c61a9816977f22b124229ffb
MHennelly <michaelthehennelly@gmail.com>
2017-04-28 22:39:34 -0700

    changes

--- src/as6/Drawing.java
+++ src/as6/Drawing.java
@@ -11,19 +11,10 @@ public class Drawing extends JPanel
 	public static final int WIDTH = 500;
 	public static final int LENGTH = 600;
 
-	private static ArrayList<Point> pointList;
-	private static ArrayList<Point> oldPoints;
+	private ArrayList<Point> pointList;
 
 	private boolean paintOn;
-	
-	private static JRadioButton greenRadioButton;
-	private static JRadioButton blueRadioButton;
-	private static JRadioButton redRadioButton;
-	private static JRadioButton eraserRadioButton;
-	private static JButton clearButton;
-	private static Color radioButtonCase = Color.WHITE;
-	
-	
+
 	public Drawing()
 	{
 		setBackground(Color.WHITE);
@@ -79,27 +70,6 @@ public class Drawing extends JPanel
 			int x = (int) point.getX();
 			int y = (int) point.getY();
 			d.fillOval(x, y, 10, 10);
-			d.setColor(radioButtonCase);
-		}
-	}
-	
-	private static class RadioButtonListener implements ActionListener {
-		public void actionPerformed(ActionEvent event) {
-			if(greenRadioButton.isSelected()) {
-				radioButtonCase = Color.GREEN;
-			} else if(blueRadioButton.isSelected()) {
-				radioButtonCase = Color.BLUE;
-			} else if(redRadioButton.isSelected()) {
-				radioButtonCase = Color.RED;
-			} else if(eraserRadioButton.isSelected()) {
-				radioButtonCase = Color.WHITE;
-			}
-		}
-	}
-	
-	private static class ClearButtonListener implements ActionListener {
-		public void actionPerformed(ActionEvent event) {
-			pointList.clear();
 		}
 	}
 
@@ -110,31 +80,10 @@ public class Drawing extends JPanel
 			public void run()
 			{
 				JFrame frame = new JFrame("Drawing");
-				frame.setLayout(new GridLayout());
 				frame.setSize(WIDTH, LENGTH);
 				Drawing panel = new Drawing();
 				frame.getContentPane().add(panel);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				greenRadioButton = new JRadioButton("Green");
-				blueRadioButton = new JRadioButton("Blue");
-				redRadioButton = new JRadioButton("Red");
-				eraserRadioButton = new JRadioButton("Eraser");
-				clearButton = new JButton("Clear");
-				frame.add(greenRadioButton, BorderLayout.SOUTH);
-				frame.add(blueRadioButton, BorderLayout.SOUTH);
-				frame.add(redRadioButton, BorderLayout.SOUTH);
-				frame.add(eraserRadioButton, BorderLayout.SOUTH);
-				frame.add(clearButton, BorderLayout.SOUTH);
-				greenRadioButton.addActionListener(new RadioButtonListener());
-				blueRadioButton.addActionListener(new RadioButtonListener());
-				redRadioButton.addActionListener(new RadioButtonListener());
-				eraserRadioButton.addActionListener(new RadioButtonListener());
-				clearButton.addActionListener(new ClearButtonListener());
-				ButtonGroup group = new ButtonGroup();
-				group.add(greenRadioButton);
-				group.add(blueRadioButton);
-				group.add(redRadioButton);
-				group.add(eraserRadioButton);
 				frame.setVisible(true);
 			}
 		});
